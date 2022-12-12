af7bfc1c0ecdfaee2fc4f48842c1ed2d0fee8d45
theo-lemoine-oc <propjhh>
2022-12-12 18:46:33 +0100

    add logo image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,19 +14,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button style={styles} textSize={20}>Button 1</Button>
         <Button style={styles}>Button 2</Button>
       </header>
     </div>
