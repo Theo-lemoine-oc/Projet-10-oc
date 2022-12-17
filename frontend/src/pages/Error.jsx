c729f0523e5560503da4eeadb5e76572c6110277
theo-lemoine-oc <propjhh>
2022-12-17 13:25:38 +0100

    create error page

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 export const Error = () => {
     return (
-        <main></main>
+        <main className="error-page">
+            <h1>404</h1>
+            <h2>Oups ! La page que vous demandez n'existe pas.</h2>
+
+            <NavLink to="/" className="return-home">Retourner sur la page d'accueil</NavLink>
+        </main>
     )
 }
 
