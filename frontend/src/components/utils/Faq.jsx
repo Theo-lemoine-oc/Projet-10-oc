f5235479c2b99ae20bc0f4c04f91356e5e0a3c72
theo-lemoine-oc <propjhh>
2022-12-26 18:46:25 +0100

    useState boolean for open or close collapse

--- frontend/src/components/utils/Faq.jsx
+++ frontend/src/components/utils/Faq.jsx
@@ -1,30 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
+
 import chevron from '../../assets/img/chevron.svg';
 import './faq.css';
 
 
 export const Faq = (props) => {
-    const isActive = (e) => {
-        e.preventDefault();
-        const button = e.target.nextSibling;
+    const [isOpened, setIsOpened] = useState(false);
+
+    function toggle(e) {
+        setIsOpened(isOpened => !isOpened);
+        
         const chevron = e.target.lastChild;
-    
-        if (!button.classList.contains("show")) {
-            button.classList.add("show");
-            chevron.classList.add("rotate");
-        } else {
-            button.classList.remove("show");
-            chevron.classList.remove("rotate");
-        }
-      };
-    
+        !chevron.classList.contains("rotate") ? chevron.classList.add("rotate") : chevron.classList.remove("rotate");
+    }
     return (
         <div className='faq-component'>
-            <div className='faq-title' id='faq-title' onClick={isActive}>
+            <div className='faq-title' id='faq-title' onClick={toggle}>
                 <h2>{ props.title }</h2>
                 <img src={chevron} alt="Chevron" />
             </div>
-            <p className='faq-content'>{ props.content }</p>
+            {isOpened && (
+                <p className='faq-content'>{ props.content }</p>
+            )}
         </div>
     )
 }
